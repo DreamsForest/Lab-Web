96078e21fdce94bc52d74225c8ac4c49b3e23ecc
Vitaly Muhin <muhinv451@gmail.com>
2023-02-17 16:41:08 +0300

    Lb-3 new Test

--- Элементы пр Web-интр_Лб№3/Скрипты3.js
+++ Элементы пр Web-интр_Лб№3/Скрипты3.js
@@ -1,302 +1,99 @@
-const questions = [
-    {
-        question: "Кто составлял большую часть декабристского восстания в 1825 году?",
-        optionA: "Священники",
-        optionB: "Ученые",
-        optionC: "Крепостные крестьяне",
-        optionD: "Дворяне",
-        correctOption: "optionD"
-    },
+let i = 0;
 
-    {
-        question: "В каком году отменили крепостное право в России ?",
-        optionA: "1861",
-        optionB: "1589",
-        optionC: "1917",
-        optionD: "1678",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "Кто стал последним императором Российской империи ?",
-        optionA: "Николай I",
-        optionB: "Николай II",
-        optionC: "Петр III",
-        optionD: "Александр II",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "Как звали жену Ленина ?",
-        optionA: "Тамара",
-        optionB: "Надежда",
-        optionC: "Анна",
-        optionD: "Елена",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "Какое государство установило иго на Руси продолжительностью почти 200 лет ?",
-        optionA: "Золотая Орда",
-        optionB: "Византийская империя",
-        optionC: "Римская империя",
-        optionD: "Османская империя",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "У какого города произошло крупнейшие сражение Отечественной войны 1812 года - Бородинское сражение в 124 километрах от Москвы ?",
-        optionA: "Смоленск",
-        optionB: "Владивосток",
-        optionC: "Уваровка",
-        optionD: "Можайск",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Что приказал Петр I брить всем боярам и дворянам 25 июня 1682 года ?",
-        optionA: "Голову",
-        optionB: "Ноги",
-        optionC: "Кошек",
-        optionD: "Бороду",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "На что повлияла военная реформа Александра II в переод с 1861 по 1874 года ?",
-        optionA: "Отслужив в армии, гарантированно получаешь землю с хозяйством",
-        optionB: "Сократился срок службы с 20 лет до 6",
-        optionC: "Эта реформа ничего нового не привнесла в Россию",
-        optionD: "Форма военных стала выглядить более презентабельно и красиво",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "Какой русский ученый был гением в области геометрии в 19 веке ?",
-        optionA: "Дмитрий Менделеев",
-        optionB: "Александр Попов",
-        optionC: "Михаил Ломоносов",
-        optionD: "Николай Лобачевский",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "В 1712 году в России была объявлена новая столица. Как называется этот город ?",
-        optionA: "Нижний Новгород",
-        optionB: "Санкт-Петербург",
-        optionC: "Таганрог",
-        optionD: "Москва",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "Эта газета стала первым печатным изданием в России. Ее утвердил Петр I, первый выпуск вышел 2 января 1703 года. ?",
-        optionA: "Из рук в руки",
-        optionB: "Ведомости",
-        optionC: "Известия",
-        optionD: "Комсомольская правда",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "C принятием этого документа в России появились такие новые должности, как коллежский регистратор и тайный советник. Что это был за документ ?",
-        optionA: "Табель о рангах",
-        optionB: "Воинский устав",
-        optionC: "Декларация независимости",
-        optionD: "Пакт Риббентропа - Молотова",
-        correctOption: "optionA"
-    },
-
-
-    {
-        question: "Какая война произошла в 18 веке в России ?",
-        optionA: "Не было никаких войн",
-        optionB: "Только русско-шведская война",
-        optionC: "Только семилетняя война",
-        optionD: "И семилетняя война и русско-шведская война",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Чем запомнился период 1762-1796 года в России ?",
-        optionA: "Восстание декабристов",
-        optionB: "Проведение великой военной реформы",
-        optionC: "Время правления императрицы Екатерины II",
-        optionD: "Период затишья перед русско-шведской войной",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "Чем прославился русский инженер Павел Яблочков ?",
-        optionA: "Построил мочт через Москву-реку",
-        optionB: "Выявил новый сорт яблок",
-        optionC: "Посторил Земский собор",
-        optionD: "Изобрел электрическую лампочку",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "С какого года Россия стала гордо называться империей ?",
-        optionA: "1648",
-        optionB: "1721",
-        optionC: "1812",
-        optionD: "1761",
-        correctOption: "optionB"
-    }
-]
-
-
-let shuffledQuestions = [] //Пустой массив для хранения перетасованных вопросов
-
-function handleQuestions() { 
-    //функция для перешивания 10 вопросов и отправка их в shuffledQuestions
-    while (shuffledQuestions.length <= 9) {
-        const random = questions[Math.floor(Math.random() * questions.length)]
-        if (!shuffledQuestions.includes(random)) {
-            shuffledQuestions.push(random)
-        }
-    }
+var otvet1 = "", otvet2 = "", otvet3 = "", otvet4 = "", otvet5 = "", otvet6 = "";
+function handleNextQuestion1(){
+    otvet1 = prompt("Ответ на первый вопрос"); //Получаем ответ пользователя через promt и передаем значение переменной
+    document.querySelector('.otvetnavopros1').innerHTML = otvet1; // через inner добавляем в дом дерево ответ пользователя на страницу
+    const element1 = document.querySelector('.otvetnavopros1'); //уже специально пригтовленный для этого див
+    element1.style.opacity = 1; //по умолчанию он принимает нулевую прозрачность но как пользователь впишет ответ сразу станет виден
+    document.querySelector('.ispravit1').innerHTML = "Исправить?";//Кнопка которая была ответить превращается в исправить и по новой запускает функцию
+}
+function handleNextQuestion2(){
+    otvet2 = prompt("Ответ на первый вопрос");
+    document.querySelector('.otvetnavopros2').innerHTML = otvet2;
+    const element1 = document.querySelector('.otvetnavopros2');
+    element1.style.opacity = 1;
+    document.querySelector('.ispravit2').innerHTML = "Исправить?";
+}
+function handleNextQuestion3(){
+    otvet3 = prompt("Ответ на первый вопрос");
+    document.querySelector('.otvetnavopros3').innerHTML = otvet3;
+    const element1 = document.querySelector('.otvetnavopros3');
+    element1.style.opacity = 1;
+    document.querySelector('.ispravit3').innerHTML = "Исправить?";
+}
+function handleNextQuestion4(){
+    otvet4 = prompt("Ответ на первый вопрос");
+    document.querySelector('.otvetnavopros4').innerHTML = otvet4;
+    const element1 = document.querySelector('.otvetnavopros4');
+    element1.style.opacity = 1;
+    document.querySelector('.ispravit4').innerHTML = "Исправить?";
+}
+function handleNextQuestion5(){
+    otvet5 = prompt("Ответ на первый вопрос");
+    document.querySelector('.otvetnavopros5').innerHTML = otvet5;
+    const element1 = document.querySelector('.otvetnavopros5');
+    element1.style.opacity = 1;
+    document.querySelector('.ispravit5').innerHTML = "Исправить?";
+}
+function handleNextQuestion6(){
+    otvet6 = prompt("Ответ на первый вопрос");
+    document.querySelector('.otvetnavopros6').innerHTML = otvet6;
+    const element1 = document.querySelector('.otvetnavopros6');
+    element1.style.opacity = 1;
+    document.querySelector('.ispravit6').innerHTML = "Исправить?";
 }
 
 
-let questionNumber = 1
-let playerScore = 0  
-let wrongAttempt = 0 
-let indexNumber = 0
 
-// функция для отображения следующего вопроса в массиве
-function NextQuestion(index) {
-    handleQuestions()
-    const currentQuestion = shuffledQuestions[index]
-    document.getElementById("question-number").innerHTML = questionNumber
-    document.getElementById("player-score").innerHTML = playerScore
-    document.getElementById("display-question").innerHTML = currentQuestion.question;
-    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
-    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
-    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
-    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
-
-}
-
-
-function checkForAnswer() {
-    const currentQuestion = shuffledQuestions[indexNumber] //получает текущий вопрос
-    const currentQuestionAnswer = currentQuestion.correctOption //получает ответ на текущий вопрос
-    const options = document.getElementsByName("option"); //получает все эелементы dom с именем 'option'
-    let correctOption = null
-
-    options.forEach((option) => {
-        if (option.value === currentQuestionAnswer) {
-            correctOption = option.labels[0].id
-        }
-    })
+function handleNextQuestion7(){
    
-    //убеждаемся что radio проверена
-    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
-        document.getElementById('option-modal').style.display = "flex"
-    }
-
-    //проверяем, совпадает ли отмеченный переключатель с ответом
-    options.forEach((option) => {
-        if (option.checked === true && option.value === currentQuestionAnswer) {
-            document.getElementById(correctOption).style.backgroundColor = "green"
-            playerScore++
-            indexNumber++
-            //задержка номера вопроса до загрузки следующего вопроса
-            setTimeout(() => {
-                questionNumber++
-            }, 1000)
+        if(otvet1 == "1721"){
+            i += 1;
+            const element11 = document.querySelector('.game-question-container1');                                                //Белая кнопка с белым фоном
+            element11.style.background = "#00FF00";
+        }
+        if(otvet1 != "1721"){
+            document.querySelector('.otvetnavopros1').innerHTML = "Это не правильный ответ. Правильный ответ - 1721";
+            const element12 = document.querySelector('.game-question-container1');                                                //Белая кнопка с белым фоном
+            element12.style.background = "#FF0000";
         }
 
-        else if (option.checked && option.value !== currentQuestionAnswer) {
-            const wrongLabelId = option.labels[0].id
-            document.getElementById(wrongLabelId).style.backgroundColor = "red"
-            document.getElementById(correctOption).style.backgroundColor = "green"
-            wrongAttempt++
-            indexNumber++
-            //задержка номера вопроса до загрузки следующего вопроса
-            setTimeout(() => {
-                questionNumber++
-            }, 1000)
+
+        if(otvet2 == "1861"){
+            i += 1;
+            const element21 = document.querySelector('.game-question-container2');                                                //Белая кнопка с белым фоном
+            element21.style.background = "#00FF00";
         }
-    })
-}
-
-
-
-//вызывается при вызове следующей кнопки
-function handleNextQuestion() {
-    checkForAnswer()
-    unCheckRadioButtons()
-    //задерживает отображение следующего вопроса на секунду
-    setTimeout(() => {
-        if (indexNumber <= 9) {
-            NextQuestion(indexNumber)
+        if(otvet2 != "1861"){
+            document.querySelector('.otvetnavopros2').innerHTML = "Это не правильный ответ. Правильный ответ - 1861";
+            const element22 = document.querySelector('.game-question-container2');                                                //Белая кнопка с белым фоном
+            element22.style.background = "#FF0000";
         }
-        else {
-            handleEndGame()
+
+
+        if(otvet3 == "1812"){
+            i += 1;
+            const element31 = document.querySelector('.game-question-container3');                                                //Белая кнопка с белым фоном
+            element31.style.background = "#00FF00";
         }
-        resetOptionBackground()
-    }, 1000);
-}
+        if (otvet3 != "1812"){
+            document.querySelector('.otvetnavopros3').innerHTML = "Это не правильный ответ. Правильный ответ - 1812";
+            const element32 = document.querySelector('.game-question-container3');                                                //Белая кнопка с белым фоном
+            element32.style.background = "#FF0000";
+        }
 
-//возвращает фон опций к нулю после отображения правильных/неправильных ответов
-function resetOptionBackground() {
-    const options = document.getElementsByName("option");
-    options.forEach((option) => {
-        document.getElementById(option.labels[0].id).style.backgroundColor = ""
-    })
-}
 
-function unCheckRadioButtons() {
-    const options = document.getElementsByName("option");
-    for (let i = 0; i < options.length; i++) {
-        options[i].checked = false;
-    }
-}
+        if(otvet4 == "988"){
+            i += 1;
+            const element41 = document.querySelector('.game-question-container4');                                                //Белая кнопка с белым фоном
+            element41.style.background = "#00FF00";
+        }
+        if(otvet4 != "988"){
+            document.querySelector('.otvetnavopros4').innerHTML = "Это не правильный ответ. Правильный ответ - 988";
+            const element42 = document.querySelector('.game-question-container4');                                                //Белая кнопка с белым фоном
+            element42.style.background = "#FF0000";
+        }
 
-// функция для ответа на все вопросы
-function handleEndGame() {
-    let remark = null
-    let remarkColor = null
 
-    //проверка состояния для замечания игрока и примечания цвета примичания
-    if (playerScore <= 3) {
-        remark = "Плохой результат."
-        remarkColor = "red"
-    }
-    else if (playerScore >= 4 && playerScore < 7) {
-        remark = "Средний результат, есть к чему стримиться."
-        remarkColor = "orange"
-    }
-    else if (playerScore >= 7) {
-        remark = "Отличный результат."
-        remarkColor = "green"
-    }
-    const playerGrade = (playerScore / 10) * 100
-
-    //данные для отображения в таблице
-    document.getElementById('remarks').innerHTML = remark
-    document.getElementById('remarks').style.color = remarkColor
-    document.getElementById('grade-percentage').innerHTML = playerGrade
-    document.getElementById('wrong-answers').innerHTML = wrongAttempt
-    document.getElementById('right-answers').innerHTML = playerScore
-    document.getElementById('score-modal').style.display = "flex"
-
-}
-
-//закрытие окно счета + сбрасывание игры
-function closeScoreModal() {
-    questionNumber = 1
-    playerScore = 0
-    wrongAttempt = 0
-    indexNumber = 0
-    shuffledQuestions = []
-    NextQuestion(indexNumber)
-    document.getElementById('score-modal').style.display = "none"
-}
-
-//функция для закрытия модального окна
-function closeOptionModal() {
-    document.getElementById('option-modal').style.display = "none"
 }
